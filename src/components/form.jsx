0d284cdedd73c0a93447e5a681102106f4146b73
erickcl23 <clerick2312@gmail.com>
2024-09-16 09:13:05 -0300

    Haciendo git pull

---
+++ src/components/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FormComponent 
+() {
+  return (
+    <div>
+        <form action="">
+            <label htmlFor="">Escriba su nombre</label>
+            <input type="text" />
+        </form>
+    </div>
+  )
+}
---
+++ src/screens/bdScreen.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const bdScreen = () => {
+  return (
+
+    <div>
+        <header>
+        <nav>
+            <ul>
+                <li>soy un header</li>
+            </ul>
+        </nav>
+        </header>
+        <main>
+    <h1>soy el body</h1>
+        </main>
+        <footer>
+            <a href="">Footer</a>
+            <a href="">footer2</a>
+            <a href="">footer3</a>
+        </footer>
+
+    </div>
+
+  )
+}
+
+
+export default bdScreen;
